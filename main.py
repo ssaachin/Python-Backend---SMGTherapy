a4a1da1ddf7dbea9358cb374a313be1de607e6cf
ssaachin <s5225374@bournemouth.ac.uk>
2023-09-29 19:11:42 +0100

    no space

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def Appointments():
         date = data.get('date')
         
         
-        time_date = f"{date} Time {time}"
+        time_date = f"{date} Time{time}"
         
         # Create a new TimeSetter instance with the modified time_date
         new_entry = TimeSetter(time_date=time_date)
